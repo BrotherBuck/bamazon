fca50a215fca6d2b88979653c18e97ae24308d93
John Dispennette <jtdartworks@gmail.com>
2016-06-17 10:10:48 -0500

    Either I'm catching on and getting this done right or I just spent an enormous amount of time working on this to no avail. I hope I'm going down the right road

--- bamazon.js
+++ bamazon.js
@@ -1,3 +1,5 @@
+
+
 var mysql = require("mysql");
 var inquirer = require('inquirer');
 
@@ -16,3 +18,46 @@ con.connect(function(err) {
    }
    console.log('Connection established');
 });
+
+//Display All of the Items available for sale. This initial 
+// display, should include the ids, names, and prices of products 
+// for sale
+
+//creating a constructor function to apply a series of values
+
+function ItemsForSale(itemID, productName, departmentName, price, stockQuantity) {
+    this.itemID  = itemID;
+    this.productName = productName;
+    this.departmentName = departmentName;
+    this.price = price;
+    this.stockQuantity = stockQuantity;
+}
+
+//creating individual items using the constructor function     
+
+var frisbee = new ItemsForSale('001','frisbee','sports', 5.00, 100);
+var widget = new ItemsForSale('002','widget','fantasy', 10.00, 50);
+var radio = new ItemsForSale('003','radio','electronics', 20.00, 25);
+var phone = new ItemsForSale('004','phone','electronics', 100.00, 15);
+var fan = new ItemsForSale('005','fan','electronics', 15.00, 75);
+var jsBook = new ItemsForSale('006','jsBook','learning', 35.00, 25);
+var toyCar = new ItemsForSale('007','toyCar','toys', 5.00, 100);
+var pen = new ItemsForSale('008','pen','stationary', 1.00, 100);
+var copyPaper = new ItemsForSale('009','copyPaper','stationary', 10.00, 75);
+var kite = new ItemsForSale('010','kite','toys', 10.00, 35);
+
+	
+
+// Users should then be prompted with two messages. The first message 
+// should ask them the ID of the product they would like to buy. 
+// The second message should ask them how many of the product they 
+// would like to buy.
+// Once the customer has placed the order: Your application should...
+// Check if your store has enough quantity of the product to meet 
+// the customer's request. If not, you should respond to the user 
+// by saying: "Insufficient quantity" and prevent the order from 
+// going through.
+// If your store DOES have enough of the product to meet the 
+// customer's request, you should fulfill their order. This means 
+// that you should show them the total cost of their puchase. 
+// Then update the SQL database to reflect the remaining quantity.
